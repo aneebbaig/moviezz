aaa76643e6579aa7f1ca02af82ddd7d1a99f4eb3
Aneeb Baig <aneeb.baig@transviti.com>
2023-01-08 17:55:18 +0500

    Added the api key

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
-const BASE_URL="https://api.themoviedb.org/3";
-const API_KEY="d1211b5b9ae71295e451140d11492f62";
-const primaryColor=Color(0xFF070818);
+
+const BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = "c734a66d8d56510c5825282dd19874ce";
+const primaryColor = Color(0xFF070818);
